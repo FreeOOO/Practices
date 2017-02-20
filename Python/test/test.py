d31b68cdb998cb24846a043e7534c53a369058a5
EE <asdertwq@126.com>
2017-02-20 21:59:06 +0800

    add testClass.py

--- Python/test/test.py
+++ Python/test/test.py
@@ -1,12 +1,49 @@
-def moda():
-    words=["1","2","3"]
-    print(words)
-    for w in words[:]:
-        print(w)
-        if int(w) > 2:
-            words.insert(0,w)
-    print(words)
-    for x in range(3,10,2):
-        print(x)
-if __name__ == "__main__":
-    moda()
+import functools
+
+def log1(text):
+    if isinstance(text,str):
+        def decorator(func):
+            @functools.wraps(func)
+            def w1(*args,**kw):
+                print('{0}'.format(text))
+                func(*args,**kw)
+            return w1
+    else:
+        @functools.wraps(text)
+        def w2(*args,**kw):
+            print('begin call')
+            text(*args,**kw)
+            print('end call')
+        return w2
+
+    return decorator
+
+def log2(func):
+    @functools.wraps(func)
+    def w2(*args,**kw):
+        print('begin call')
+        func(*args,**kw)
+        print('end call')
+    return w2
+
+def log3(func):
+    print('begin call===')
+    func()
+
+@log1('test decorator')
+def now(text):
+    print('{0:.9s}test'.format(text))
+
+@log2
+def now2():
+    print('===')
+
+@log3
+def now3():
+    print('-------')
+
+if __name__ == '__main__':
+    now('2016')
+    f = now2
+    f()
+    now3
---
+++ Python/test/testClass.py
@@ -0,0 +1,25 @@
+class Aminal(object):
+    #def __init__(self,name):
+    #    self.name = name
+    #def run(self):
+    #    print('{0} is running'.format(self.name))
+    def printInfo(self):
+        print('aminal is running')
+
+class Dog(Aminal):
+    def __init__(self,name):
+        self.__name = name
+    def printInfo(self):
+        print('{0} is running'.format(self.__name))
+    #def __init__(self,eat):
+    #    Aminal.__init__(self,'dog')
+    #    self.__eat = eat
+    #def printInfo(self):
+    #    print('{0} {1}'.format(self.name,self.__eat))
+
+if __name__ == '__main__':
+    dog = Dog('dog')
+    #dog.run()
+    dog.printInfo()
+    print(getattr(dog,'_Dog__name'))
+    #print(dog.name)
